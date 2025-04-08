cd43465b19181cc7786497f297ddbfa3c29647b7
Tereza Michalikova <michalikova.tereza@seznam.cz>
2025-04-08 18:46:53 +0200

    Hotova bublanina

--- src/main/java/cz/czechitas/recept/SpousteciTrida.java
+++ src/main/java/cz/czechitas/recept/SpousteciTrida.java
@@ -14,42 +14,70 @@ import cz.czechitas.recept.suroviny.Vajicka;
 
 public class SpousteciTrida {
 
-  public static void main(String[] args) {
-    Miska cervenaMiska;
-    Miska zlutaMiska;
-    Mixer mixer;
-    Vaha kuchynskaVaha;
-    PlechNaPeceni plech;
-    ElektrickaTrouba trouba;
-    Vajicka vajicka;
+    public static void main(String[] args) {
+        Miska cervenaMiska;
+        Miska zlutaMiska;
+        Mixer mixer;
+        Vaha kuchynskaVaha;
+        PlechNaPeceni plech;
+        ElektrickaTrouba trouba;
+        Vajicka vajicka;
 
-    Ovoce ovoce;
-    Maslo maslo125g;
-    Mouka pytlikMouky;
-    Cukr pytlikCukru;
-    PrasekDoPeciva prasekDoPeciva;
+        Ovoce ovoce;
+        Maslo maslo125g;
+        Mouka pytlikMouky;
+        Cukr pytlikCukru;
+        PrasekDoPeciva prasekDoPeciva;
 
-    cervenaMiska = new Miska("cervenaMiska");
-    zlutaMiska = new Miska("zlutaMiska");
-    mixer = new Mixer("mixer");
-    kuchynskaVaha = new Vaha("vaha");
-    plech = new PlechNaPeceni("plech");
-    trouba = new ElektrickaTrouba("trouba");
+        cervenaMiska = new Miska("cervenaMiska");
+        zlutaMiska = new Miska("zlutaMiska");
+        mixer = new Mixer("mixer");
+        kuchynskaVaha = new Vaha("vaha");
+        plech = new PlechNaPeceni("plech");
+        trouba = new ElektrickaTrouba("trouba");
 
-    vajicka = new Vajicka("vajicka");
-    ovoce = new Ovoce("ovoce");
-    maslo125g = new Maslo("maslo125g");
-    pytlikMouky = new Mouka("pytlikMouky");
-    pytlikCukru = new Cukr("pytlikCukru");
-    prasekDoPeciva = new PrasekDoPeciva("prasekDoPeciva");
+        vajicka = new Vajicka("vajicka");
+        ovoce = new Ovoce("ovoce");
+        maslo125g = new Maslo("maslo125g");
+        pytlikMouky = new Mouka("pytlikMouky");
+        pytlikCukru = new Cukr("pytlikCukru");
+        prasekDoPeciva = new PrasekDoPeciva("prasekDoPeciva");
 
-    //---------------------------------------------------------------------
+        //---------------------------------------------------------------------
 
-    // TODO: Sem napiste recept na bublaninu
-    // Pouzivejte napovidani v editoru.
-    // Vyskakuje samo nebo pomoci Ctrl+Mezernik
+        // TODO: Sem napiste recept na bublaninu
+        // Pouzivejte napovidani v editoru.
+        // Vyskakuje samo nebo pomoci Ctrl+Mezernik
 
-    cervenaMiska.nalozSiJedenKus(vajicka);
-  }
 
+
+       for (int i = 0; i < 4; i++) {
+            cervenaMiska.nalozSiJedenKus(vajicka);
+        }
+        cervenaMiska.nalozSiCelyObsah(pytlikCukru);
+        mixer.zamichej(cervenaMiska);
+        cervenaMiska.nalozSiCelyObsah(maslo125g);
+        mixer.zamichej(cervenaMiska);
+        kuchynskaVaha.vynulujSeS(zlutaMiska);
+        while (kuchynskaVaha.zjistiHmotnost(zlutaMiska) != 250) {
+            if (kuchynskaVaha.zjistiHmotnost(zlutaMiska) < 250) {
+                zlutaMiska.nalozSiTrochu(pytlikMouky);
+            } else if (kuchynskaVaha.zjistiHmotnost(zlutaMiska) > 250) {
+                zlutaMiska.vylozSiTrochu();
+            }
+        }
+        cervenaMiska.nalozSiObsahJineMisky(zlutaMiska);
+        cervenaMiska.nalozSiCelyObsah(prasekDoPeciva);
+        mixer.zamichej(cervenaMiska);
+        plech.preberSiObsah(cervenaMiska);
+        for (int i = 0; i < 50; i++) {
+            plech.posypSeKusem(ovoce);
+        }
+        trouba.zapniSe(180);
+        trouba.nechejPect(5);
+        trouba.vlozSiDovnitr(plech);
+        trouba.nechejPect(25);
+        trouba.vypniSe();
+        trouba.vyndejObsahVen();
+    }
 }
